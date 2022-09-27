1975c1c4f6590086605ff326e4a5965aab537a44
LuisEduNasc <luis.nacimento@cravehq.com>
2022-09-27 08:35:07 -0300

    feat(component): react component logic to check and uncheck squares

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const Square: React.FC<{
+  position: number,
+  handleClick: (position: number) => void,
+  clicked: boolean
+}> = ({position, handleClick, clicked}) => {
+  return (
+    <li className={`square-li position-${position} ${clicked ? 'clicked' : ''}`}>
+      <button onClick={() => handleClick(position)}></button>
+    </li>
+  );
+};
+
+const SquareContainer: React.FC<{squares: Array<number>}> = ({squares}) => {
+  const [squaresState, setSquaresState] = React.useState<Array<number>>([]);
+
+  const isClicked = (position: number): boolean => {
+    return squaresState.findIndex((item) => item === position) === -1 ? false : true;
+  };
+
+  const handleSquareClicked = (position: number) => {
+    setSquaresState([...squaresState, position]);
+  };
+
+  const removeSquarePromise = React.useCallback((idx: number): Promise<Array<number>> => {
+    return new Promise((resolve, _reject) => {
+      setTimeout(() => {
+        const newSquaresArray = squaresState;
+        newSquaresArray.pop();
+        resolve(newSquaresArray);
+      }, 1000 * (idx + 1));
+    });
+  }, [squaresState]);
+
+  const removeClickedSquares = React.useCallback(() => {
+    let timeOut;
+    squaresState.forEach(async (ele, idx) => {
+      const remainingArray: Array<number> = await removeSquarePromise(idx);
+      setSquaresState([...remainingArray]);
+    });
+
+    clearInterval(timeOut);
+  }, [squaresState, removeSquarePromise]);
+
+  React.useEffect(() => {
+    if (squaresState.length === squares.length) {
+      removeClickedSquares();
+    }
+  }, [squaresState.length, squares.length, removeClickedSquares, removeSquarePromise]);
+
+  return (
+    <ul className='square-ul'>
+      {
+       squares.map((item: number) => (
+          <Square
+            key={item}
+            position={item}
+            handleClick={handleSquareClicked}
+            clicked={isClicked(item)}
+          />
+        ))
+      }
+    </ul>
+  )
+};
 
 function App() {
+  const squaresArray = [1, 2, 3, 4, 5, 6, 7];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SquareContainer squares={squaresArray}/>
     </div>
   );
 }
